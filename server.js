ea149ea4d842ef114d8cc913ea257fc2a7977eac
yonsei-sangnam <sangnam@yonsei.ac.kr>
2026-05-13 09:52:28 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const auth = require('./auth');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
